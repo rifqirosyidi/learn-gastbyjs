0ce61f14eed4066b627b4a259771250048a268da
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-11-04 16:54:59 +0700

    rendering source content

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql, useStaticQuery } from 'gatsby'
 
 
 function Blog() {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <div>
             <Layout>
-            
-            <h1>Blog</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, nobis omnis! Perspiciatis fugit </p>
-
+                
+                <h1>Blog</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, nobis omnis! Perspiciatis fugit </p>
+                
+                <ol>
+                    {data.allMarkdownRemark.edges.map((edge) => {
+                        return (
+                            <li>
+                                <h2>{ edge.node.frontmatter.title }</h2>
+                                <p>{ edge.node.frontmatter.date }</p>
+                            </li>
+                        )
+                    })}
+                </ol>
             </Layout>
         </div>
     )
