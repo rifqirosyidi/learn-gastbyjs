2627cef57f1490a44fe31151dc3b5af15294a002
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-11-05 12:34:59 +0700

    styling

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { graphql, useStaticQuery, Link } from 'gatsby'
-
+import blogStyles from './blog.module.scss'
 
 function Blog() {
     const data = useStaticQuery(graphql`
@@ -29,14 +29,14 @@ function Blog() {
                 <h1>Blog</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, nobis omnis! Perspiciatis fugit </p>
                 
-                <ol>
+                <ol className={ blogStyles.posts }>
                     { data.allMarkdownRemark.edges.map((edge) => {
                         return (
-                            <li>
+                            <li className={ blogStyles.post }>
                                 <Link to={ `/blog/${edge.node.fields.slug}` } >
                                 <h2>{ edge.node.frontmatter.title }</h2>
-                                </Link>
                                 <p>{ edge.node.frontmatter.date }</p>
+                                </Link>    
                             </li>
                         )
                     })}
